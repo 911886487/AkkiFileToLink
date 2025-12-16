a8487aad3b97fc6b5e2930af4ad0124d2f780723
Akki <129473687+911886487@users.noreply.github.com>
2025-12-16 05:38:15 +0000

    Refactor start_help.py to improve command handling and logging functionality

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -1,111 +1,131 @@
-# (c) Adarsh-Goel
-
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-
+#Adarsh goel
 from Adarsh.bot import StreamBot
 from Adarsh.vars import Var
-from Adarsh.utils.database import db
-from Adarsh.utils.human_readable import humanbytes
+import logging
+logger = logging.getLogger(__name__)
 from Adarsh.bot.plugins.stream import MY_PASS
+from Adarsh.utils.human_readable import humanbytes
+from Adarsh.utils.database import Database
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
 from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
+from Adarsh.utils.database import db
+from pyrogram.types import ReplyKeyboardMarkup
 
-
-# ================= SAFE LOG FUNCTION =================
-
-async def safe_log(bot, text):
-    """
-    Send log message to BIN_CHANNEL safely
-    (will NEVER crash bot)
-    """
-    if not Var.BIN_CHANNEL:
-        return
-    try:
-        await bot.send_message(Var.BIN_CHANNEL, text)
-    except Exception:
-        # ignore all BIN_CHANNEL errors
-        pass
-
-
-# ================= START COMMAND =================
-
-@StreamBot.on_message(filters.command("start") & filters.private)
-async def start_handler(b, m: Message):
-
-    # ---- Save user ----
+                      
+StreamBot.on_message(filters.command('start') & filters.private)
+async def start(b, m):
     if not await db.is_user_exist(m.from_user.id):
         await db.add_user(m.from_user.id)
-        await safe_log(
-            b,
-            f"#NEW_USER\n\n"
-            f"Name: [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n"
-            f"ID: `{m.from_user.id}`"
+        await b.send_message(
+            Var.BIN_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{m.from_user.first_name}](tg://user?id={m.from_user.id}) Started !!"
         )
-
     usr_cmd = m.text.split("_")[-1]
-
-    # ---- Normal /start ----
     if usr_cmd == "/start":
         await m.reply_photo(
-            photo="https://files.catbox.moe/he6d75.jpg
-",
-            caption=(
-                "**ʜᴇʟʟᴏ ⚡**\n\n"
-                "ɪ ᴀᴍ ᴀ **ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇ / ᴠɪᴅᴇᴏ ᴛᴏ ᴘᴇʀᴍᴀɴᴇɴᴛ "
-                "ʟɪɴᴋ & sᴛʀᴇᴀᴍ ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ.**\n\n"
-                "**Use /help for more details**\n\n"
-                "**Send me any file / video to see my power ⚡**"
-            ),
+            photo="https://files.catbox.moe/he6d75.jpg",
+
+            caption="**ʜᴇʟʟᴏ...⚡\n\nɪᴀᴍ ᴀ sɪᴍᴘʟᴇ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇ/ᴠɪᴅᴇᴏ ᴛᴏ ᴘᴇʀᴍᴀɴᴇɴᴛ ʟɪɴᴋ ᴀɴᴅ sᴛʀᴇᴀᴍ ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ.**\n\n**ᴜsᴇ /help ғᴏʀ ᴍᴏʀᴇ ᴅᴇᴛsɪʟs\n\nsᴇɴᴅ ᴍᴇ ᴀɴʏ ᴠɪᴅᴇᴏ / ғɪʟᴇ ᴛᴏ sᴇᴇ ᴍʏ ᴘᴏᴡᴇʀᴢ...**",
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [
-                        InlineKeyboardButton("⚡ UPDATES ⚡", url="https://t.me/pirateakki"),
-                        InlineKeyboardButton("⚡ SUPPORT ⚡", url="https://t.me/Premium_Plan_buyer_Bot"),
-                    ],
-                    [
-                        InlineKeyboardButton("OWNER", url="https://t.me/Premium_Plan_buyer_Bot"),
-                        InlineKeyboardButton("💠 DEVELOPER", url="https://t.me/pirateakki"),
-                    ],
+                    [InlineKeyboardButton("⚡ UPDATES ⚡", url="https://t.me/pirateakki"), InlineKeyboardButton("⚡ SUPPORT ⚡", url="https://t.me/beta_support")],
+                    [InlineKeyboardButton("OWNER", url="https://t.me/Premium_Plan_buyer_Bot"), InlineKeyboardButton("💠 DEVELOPER", url="https://github.com/Adarsh-Goel")],
+                    [InlineKeyboardButton("💌 SUBSCRIBE 💌", url="https://t.me/official_RATNA")]
                 ]
             ),
+            
         )
-        return
-
-    # ---- Start with file id ----
-    try:
-        get_msg = await b.get_messages(chat_id=Var.BIN_CHANNEL, ids=int(usr_cmd))
-    except Exception:
-        return await m.reply_text("❌ Invalid or expired link.")
-
-    if get_msg.video:
-        file_size = humanbytes(get_msg.video.file_size)
-        file_name = get_msg.video.file_name
-    elif get_msg.document:
-        file_size = humanbytes(get_msg.document.file_size)
-        file_name = get_msg.document.file_name
-    elif get_msg.audio:
-        file_size = humanbytes(get_msg.audio.file_size)
-        file_name = get_msg.audio.file_name
     else:
-        return await m.reply_text("❌ Unsupported file type.")
 
-    stream_link = (
-        f"https://{Var.FQDN}/{get_msg.id}"
-        if Var.ON_HEROKU or Var.NO_PORT
-        else f"http://{Var.FQDN}:{Var.PORT}/{get_msg.id}"
-    )
+        get_msg = await b.get_messages(chat_id=Var.BIN_CHANNEL, ids=int(usr_cmd))
 
-    msg_text = (
-        "**⚡ Your link is generated!**\n\n"
-        f"📂 **File name:** `{file_name}`\n"
-        f"📦 **File size:** `{file_size}`\n\n"
-        f"🔗 **Download link:** {stream_link}\n\n"
-        "♻️ This link is permanent and won’t expire"
-    )
+        file_size = None
+        if get_msg.video:
+            file_size = f"{humanbytes(get_msg.video.file_size)}"
+        elif get_msg.document:
+            file_size = f"{humanbytes(get_msg.document.file_size)}"
+        elif get_msg.audio:
+            file_size = f"{humanbytes(get_msg.audio.file_size)}"
 
-    await m.reply_text(
-        msg_text,
+        file_name = None
+        if get_msg.video:
+            file_name = f"{get_msg.video.file_name}"
+        elif get_msg.document:
+            file_name = f"{get_msg.document.file_name}"
+        elif get_msg.audio:
+            file_name = f"{get_msg.audio.file_name}"
+
+        stream_link = "https://{}/{}".format(Var.FQDN, get_msg.id) if Var.ON_HEROKU or Var.NO_PORT else \
+            "http://{}:{}/{}".format(Var.FQDN,
+                                     Var.PORT,
+                                     get_msg.id)
+
+        msg_text = "**ᴛᴏᴜʀ ʟɪɴᴋ ɪs ɢᴇɴᴇʀᴀᴛᴇᴅ...⚡\n\n📧 ғɪʟᴇ ɴᴀᴍᴇ :-\n{}\n {}\n\n💌 ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ :- {}\n\n♻️ ᴛʜɪs ʟɪɴᴋ ɪs ᴘᴇʀᴍᴀɴᴇɴᴛ ᴀɴᴅ ᴡᴏɴ'ᴛ ɢᴇᴛ ᴇxᴘɪʀᴇᴅ ♻️\n\n<b>❖ YouTube.com/@itzjeol</b>**"
+        await m.reply_text(            
+            text=msg_text.format(file_name, file_size, stream_link),
+            
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡ ᴅᴏᴡɴʟᴏᴀᴅ ɴᴏᴡ ⚡", url=stream_link)]])
+        )
+
+
+@StreamBot.on_message(filters.command('help') & filters.private)
+async def help_handler(bot, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
+        await bot.send_message(
+            Var.BIN_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) Started !!"
+        )
+              
+    await message.reply_photo(
+            photo="https://files.catbox.moe/he6d75.jpg",
+            caption="**┣⪼ sᴇɴᴅ ᴍᴇ ᴀɴʏ ғɪʟᴇ/ᴠɪᴅᴇᴏ ᴛʜᴇɴ ɪ ᴡɪʟʟ ʏᴏᴜ ᴘᴇʀᴍᴀɴᴇɴᴛ sʜᴀʀᴇᴀʙʟᴇ ʟɪɴᴋ ᴏғ ɪᴛ...\n\n┣⪼ ᴛʜɪs ʟɪɴᴋ ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴏʀ ᴛᴏ sᴛʀᴇᴀᴍ ᴜsɪɴɢ ᴇxᴛᴇʀɴᴀʟ ᴠɪᴅᴇᴏ ᴘʟᴀʏᴇʀs ᴛʜʀᴏᴜɢʜ ᴍʏ sᴇʀᴠᴇʀs.\n\n┣⪼ ғᴏʀ sᴛʀᴇᴀᴍɪɴɢ ᴊᴜsᴛ ᴄᴏᴘʏ ᴛʜᴇ ʟɪɴᴋ ᴀɴᴅ ᴘᴀsᴛᴇ ɪᴛ ɪɴ ʏᴏᴜʀ ᴠɪᴅᴇᴏ ᴘʟᴀʏᴇʀ ᴛᴏ sᴛᴀʀᴛ sᴛʀᴇᴀᴍɪɴɢ.\n\n┣⪼ ᴛʜɪs ʙᴏᴛ ɪs ᴀʟsᴏ sᴜᴘᴘᴏʀᴛ ɪɴ ᴄʜᴀɴɴᴇʟ. ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀs ᴀᴅᴍɪɴ ᴛᴏ ɢᴇᴛ ʀᴇᴀʟᴛɪᴍᴇ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ ғᴏʀ ᴇᴠᴇʀʏ ғɪʟᴇs/ᴠɪᴅᴇᴏs ᴘᴏsʏ../\n\n┣⪼ ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ :- /about\n\n\nᴘʟᴇᴀsᴇ sʜᴀʀᴇ ᴀɴᴅ sᴜʙsᴄʀɪʙᴇ**", 
+  
+        
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("⚡ DOWNLOAD NOW ⚡", url=stream_link)]]
-        ),
-    )
+                [
+                    [InlineKeyboardButton("⚡ UPDATES ⚡", url="https://t.me/pirateakki"), InlineKeyboardButton("⚡ SUPPORT ⚡", url="https://t.me/Premium_Plan_buyer_Bot")],
+                    [InlineKeyboardButton("OWNER", url="https://t.me/Premium_Plan_buyer_Bot"), InlineKeyboardButton("💠 DEVELOPER", url="https://t.me/Premium_Plan_buyer_Bot")],
+                    [InlineKeyboardButton("💌 SUBSCRIBE 💌", url="https://t.me/official_RATNA")]
+                ]
+            ),
+            
+        )
+
+@StreamBot.on_message(filters.command('about') & filters.private)
+async def about_handler(bot, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
+        await bot.send_message(
+            Var.BIN_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) Started !!"
+        )
+    await message.reply_photo(
+            photo="https://files.catbox.moe/he6d75.jpg",
+            caption="""<b>sᴏᴍᴇ ʜɪᴅᴅᴇɴ ᴅᴇᴛᴀɪʟs😜</b>
+
+<b>╭━━━━━━━〔ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ〕</b>
+┃
+┣⪼<b>ʙᴏᴛ ɴᴀᴍᴇ : ғɪʟᴇ ᴛᴏ ʟɪɴᴋ
+┣⪼<b>ᴜᴘᴅᴀᴛᴇᴢ : <a href='https://t.me/pirateakki'>akki ji</a></b>
+┣⪼<b>sᴜᴘᴘᴏʀᴛ : <a href='https://t.me/Premium_Plan_buyer_Bot'>akki support</a></b>
+┣⪼<b>sᴇʀᴠᴇʀ : ʜᴇʀᴜᴋᴏ</b>
+┣⪼<b>ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ</b>
+┣⪼<b>ʟᴀɴɢᴜᴀɢᴇ: ᴘʏᴛʜᴏɴ 3</b>
+┣⪼<b>akki info : <a href='https://t.me/official_RATNA'>Akki here</a></b>
+┃
+<b>╰━━━━━━━〔ᴘʟᴇᴀsʀ sᴜᴘᴘᴏʀᴛ〕</b>""",
+  
+        
+        reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("⚡ UPDATES ⚡", url="https://t.me/pirateakki"), InlineKeyboardButton("⚡ SUPPORT ⚡", url=""
+                    "https://t.me/Premium_Plan_buyer_Bot")],
+                    [InlineKeyboardButton("OWNER", url="https://t.me/Premium_Plan_buyer_Bot"), InlineKeyboardButton("💠 DEVELOPER", url="https://t.me/pirateakki")],
+                    [InlineKeyboardButton("💌 SUBSCRIBE 💌", url="https://t.me/pirateakki")]
+                ]
+            ),
+            
+        )
